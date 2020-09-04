ab1991952e59df1bc4bf86aa528aeabee5cf94ef
Tin Vo <tinnvo1101@gmail.com>
2020-09-03 21:13:34 -0400

    added testing script for pushing to cloud

--- scan.sh
+++ scan.sh
@@ -1,5 +1,8 @@
 SOURCE_DIR=/Users/tinnvo/Documents/Sources/Release
 
+
+
+
 for PROJECT in $(cat projects.list); do
   echo "START - PREPARING PROJECT ${PROJECT}..."
   PROJECT_PATH=$SOURCE_DIR/${PROJECT}
@@ -9,5 +12,9 @@ for PROJECT in $(cat projects.list); do
   echo "GENERATING ${PROJECT_NAME}.irx..."
   appscan.sh prepare -c appscan-config-tmp.xml -n $PROJECT_NAME.irx
   echo "END - PREPARING PROJECT ${PROJECT}..."
+
+  # echo "START - UPLOADING $PROJECT_NAME.irx FILE..."
+  # curl -X POST --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' --header 'Authorization: Bearer <API_KEY>' -F fileName=$PROJECT_NAME.irx 'https://cloud.appscan.com/api/v2/FileUpload'
+
   echo ""
 done
