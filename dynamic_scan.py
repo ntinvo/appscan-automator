7b3fa5d6c8a947f986ce7fe5d5fa2cda71d4868b
Tin Vo <tinnvo1101@gmail.com>
2020-10-06 18:38:04 -0400

    added logging

--- dynamic_scan.py
+++ dynamic_scan.py
@@ -1,4 +1,9 @@
+import logging
 import os
+import subprocess
+import sys
+import coloredlogs
+
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -6,6 +11,54 @@ dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 API_KEY = os.environ.get("API_KEY")
+
+main_logger = logging.getLogger(__name__)
+
+
+def run_subprocess(command, timeout=None, logger=None):
+    popen = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    lines_iterator = iter(popen.stdout.readline, b"")
+    output = ""
+    while popen.poll() is None:
+        for line in lines_iterator:
+            nline = line.rstrip()
+            nline = nline.decode(
+                encoding=sys.stdout.encoding,
+                errors="replace" if (sys.version_info) < (3, 5) else "backslashreplace",
+            )
+            output += nline + "\n"
+            if logger:
+                logger.info(nline)
+            if main_logger.level <= logging.INFO:
+                if not logger:
+                    print(nline, end="\r\n", flush=True)
+    if logger:
+        logger.info(f"PROCESS: {popen.pid} return {popen.returncode}")
+        if popen.returncode != 0:
+            _, err = popen.communicate()
+            logger.error(f"ERROR: {err}")
+            raise Exception(err)
+    retval = popen.wait(timeout)
+    subprocess.run(["stty", "sane"])
+    return retval, output
+
+
+def setup_main_logging(verbose=logging.INFO):
+    """Set the logging level for the script that the user passes in. Default is WARNING
+    
+    Args:
+        verbose: the log level ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
+    Returns:
+        None
+    Raises:
+        None
+    """
+    coloredlogs.install(
+        level=verbose,
+        logger=main_logger,
+        fmt="%(asctime)s %(hostname)s %(name)s[%(process)d] %(levelname)s %(message)s",
+    )
+
 
 headers = {
     "Content-Type": "application/json",
@@ -33,11 +86,13 @@ data = {
     "Personal": "false",
 }
 
-import requests
+# import requests
 
-res = requests.post(
-    "https://cloud.appscan.com/api/v2/Scans/DynamicAnalyzer", json=data, headers=headers
-)
+# res = requests.post(
+#     "https://cloud.appscan.com/api/v2/Scans/DynamicAnalyzer", json=data, headers=headers
+# )
+setup_main_logging()
+run_subprocess("ls -al", logger=main_logger)
 
-print(res.text)
+# print(res.text)
 
