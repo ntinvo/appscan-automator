0d6f80b10f5deff598e7d3f1836808c40b0529ed
Tin Vo <tinnvo1101@gmail.com>
2021-07-28 11:02:07 -0400

    removed jazz settings

--- settings.py
+++ settings.py
@@ -9,8 +9,5 @@ load_dotenv(dotenv_path)
 
 KEY_ID = os.environ.get("KEY_ID")
 KEY_SECRET = os.environ.get("KEY_SECRET")
-JAZZ_REPO = os.environ.get("JAZZ_REPO")
-JAZZ_USER = os.environ.get("JAZZ_USER")
-JAZZ_PASS = os.environ.get("JAZZ_PASS")
 JFROG_APIKEY = os.environ.get("JFROG_APIKEY")
 JENKINS_TAAS_TOKEN = os.environ.get("JENKINS_TAAS_TOKEN")
